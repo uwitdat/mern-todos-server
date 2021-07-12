36fb39cbc1d06f0f7b545850430fe57821ad3d64
Ben Shekhtman <benshekhtman@hotmail.com>
2021-07-12 12:46:26 -0400

    deploy

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
 })
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 5000
 mongoose.connect(process.env.db,
     {
         useNewUrlParser: true,
